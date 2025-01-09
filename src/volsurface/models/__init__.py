c2bf83cc83dcc926605e296124cd6bcf1635f71f
Rob Konstanty-Nicholson <robgknicholson@gmail.com>
2025-01-10 00:14:08 +1100

    fix models init

--- src/volsurface/models/__init__.py
+++ src/volsurface/models/__init__.py
@@ -1,1 +1,7 @@
-from . import sabr
+from .base import ModelParameters, VolatilityModel
+from .custom_sabr import CustomSABRModel, SABRParameters
+from .quantlib_sabr import QuantLibSABRModel, QuantLibSABRParameters
+from .factory import ModelFactory, ModelType
+
+# For backward compatibility (in case other code is still using the old import)
+from .custom_sabr import CustomSABRModel as SABRModel
